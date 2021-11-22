287b2653c2b35ac5ffecc49fd6beadfcd75016cb
nbn <nbn1209@126.com>
2021-11-22 22:33:18 +0800

    feat: [array map]

---
+++ basic/__test__/array.map.spec.js
@@ -0,0 +1,18 @@
+
+import { expect, it, test } from '@jest/globals'
+import '../array.map'
+
+
+
+it('自定义map', () => {
+  const arr = [1, 2, 3]
+  const trans = arr.myMap((item, index) => `${item}-${index}`)
+  expect(trans).toStrictEqual(['1-0', '2-1', '3-2'])
+})
+
+it('原始map', () => {
+  const arr = [1, 2, 3]
+
+  const trans = arr.map((item, index) => `${item}-${index}`)
+  expect(trans).toStrictEqual(['1-0', '2-1', '3-2'])
+})
---
+++ basic/array.map.js
@@ -0,0 +1,8 @@
+Array.prototype.myMap = function (fn) {
+  const arr = this
+  const result = []
+  for (let i = 0; i < arr.length; i++) {
+    result.push(fn(arr[i], i))
+  }
+  return result
+}
